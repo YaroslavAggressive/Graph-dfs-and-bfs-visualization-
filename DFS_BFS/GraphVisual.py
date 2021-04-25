d72860174dbd563193dc77d6548d08d74c832b8b
YaroslavAggressive <55752143+YaroslavAggressive@users.noreply.github.com>
2021-04-26 02:49:24 +0500

    Updated tests and fixed error in dfa-method

--- DFS_BFS/GraphVisual.py
+++ DFS_BFS/GraphVisual.py
@@ -6,35 +6,37 @@ class MyGraph:
 
     def __init__(self, graph: dict):
         if isinstance(graph, dict):
-            self.graph = nwx.MultiGraph(graph)
+            self.graph = nwx.Graph(graph)
             self.vertices = self.graph.nodes  # двумерная матрица смежности
-            self.graph = graph
-        else:
-            return None  # в конструктор передан неверный тип данных для построения графа
 
-    def DFS(self, start_node):  #  в глубину
-        nodes_visited = {node:False for node in self.graph.nodes}
-        nodes_stack = deque(start_node)
+    def DFS(self, start_node):  # в глубину
+        nodes_colors = {node:'white' for node in self.graph.nodes}
+        nodes_stack = [start_node]
+        result = []
+
+        def dfs_recursive(node: str):
+            nodes_colors[node] = 'gray'
+            result.append(node)
+            for node_tmp in self.graph.neighbors(node):
+                if nodes_colors[node_tmp] == 'white':
+                    dfs_recursive(node_tmp)
+
+            nodes_colors[node] = 'black'
+
+        dfs_recursive(start_node)
+
+        return result
+
+    def BFS(self, start_node):  # в ширину
+        nodes_visited = {node: False for node in self.graph.nodes}
+        nodes_stack = [start_node]
         result = []
         while nodes_stack:
-            curr_node = nodes_stack.pop()
+            curr_node = nodes_stack.pop(0)
             if not nodes_visited[curr_node]:
                 result.append(curr_node)
                 nodes_visited[curr_node] = True
             for node in self.graph.neighbors(curr_node):
                 if not nodes_visited[node]:
                     nodes_stack.append(node)
-        return result
-
-    def BFS(self, start_node):  #  в ширину
-        nodes_visited = {node: False for node in self.graph.nodes}
-        nodes_stack = deque(start_node)
-        result = []
-        while nodes_stack:
-            curr_node = nodes_stack.pop()
-            result.append(curr_node)
-            for node in self.graph.neighbors(curr_node):
-                if not nodes_visited[node]:
-                    nodes_stack.append(node)
-                    nodes_visited[node] = True
         return result
--- DFS_BFS/tests.py
+++ DFS_BFS/tests.py
@@ -14,133 +14,91 @@ class Case:
         return 'test_{}'.format(self._name)
 
 
-G_1 = nwx.MultiGraph({'A': ['C', 'D'],
-                      'B': [],
-                      'C': ['B', 'D'],
-                      'D': ['A', 'C', 'E'],
-                      'E': ['A', 'D']})
+dict_1 = {'A': ['C', 'D'],
+          'B': [],
+          'C': ['B', 'D'],
+          'D': ['A', 'C', 'E'],
+          'E': ['A', 'D']}
+G_1 = nwx.Graph(dict_1)
 res_1 = list(nwx.algorithms.traversal.dfs_tree(G_1, 'A'))
 
-G_2 = nwx.MultiGraph({'A': ['C', 'D'],
-                      'B': ['A', 'S'],
-                      'C': ['B', 'D'],
-                      'D': ['A', 'C', 'E'],
-                      'E': ['A', 'D']})
+dict_2 = {'A': ['C', 'D'],
+          'B': ['A', 'S'],
+          'C': ['B', 'D'],
+          'D': ['A', 'C', 'E'],
+          'E': ['A', 'D']}
+G_2 = nwx.Graph(dict_2)
 res_2 = list(nwx.algorithms.traversal.dfs_tree(G_2, 'A'))
 
-G_3 = nwx.MultiGraph({'A': ['C', 'D'],
-                      'B': ['A', 'S'],
-                      'C': ['B', 'Y'],
-                      'D': ['A', 'C', 'A'],
-                      'E': []})
-res_3 = list(nwx.algorithms.traversal.dfs_tree(G_3, 'C'))
+dict_3 = {'A': ['C', 'D'],
+          'B': ['A', 'S'],
+          'C': ['B', 'Y'],
+          'D': ['A', 'C', 'A'],
+          'E': []}
+G_3 = nwx.Graph(dict_3)
+res_3 = list(nwx.algorithms.traversal.dfs_tree(G_3, 'A'))
 
-G_4 = nwx.MultiGraph({'A': ['D'],
-                      'B': ['A', 'S'],
-                      'C': ['B', 'D'],
-                      'D': ['A', 'C', 'E'],
-                      'E': ['A', 'D']})
+dict_4 = {'A': ['D'],
+          'B': ['A', 'S'],
+          'C': ['B', 'D'],
+          'D': ['A', 'C', 'E'],
+          'E': ['A', 'D']}
+G_4 = nwx.Graph(dict_4)
 res_4 = list(nwx.algorithms.traversal.dfs_tree(G_4, 'A'))
 
-G_5 = nwx.MultiGraph({'A': ['C', 'D'],
-                      'B': ['A', 'S'],
-                      'C': ['B', 'D'],
-                      'D': ['B', 'B', 'E'],
-                      'E': ['A', 'D']})
+dict_5 = {'A': ['C', 'D'],
+          'B': ['A', 'S'],
+          'C': ['B', 'D'],
+          'D': ['B', 'B', 'E'],
+          'E': ['A', 'D']}
+G_5 = nwx.Graph(dict_5)
 res_5 = list(nwx.algorithms.traversal.dfs_tree(G_5, 'A'))
 
-G_6 = nwx.MultiGraph({'A': ['C', 'D'],
-                      'B': ['A', 'S'],
-                      'C': ['B', 'D'],
-                      'D': ['A', 'C', 'E'],
-                      'E': ['A']})
+dict_6 = {'A': ['C', 'D'],
+          'B': ['A', 'S'],
+          'C': ['B', 'D'],
+          'D': ['A', 'C', 'E'],
+          'E': ['A']}
+G_6 = nwx.Graph(dict_6)
 res_6 = list(nwx.algorithms.traversal.dfs_tree(G_6, 'A'))
 
-TEST_CASES_FOR_BFS = [
-    Case(name='№1', input={'A': ['C', 'D'],
-                           'B': [],
-                           'C': ['B', 'D'],
-                           'D': ['A', 'C', 'E'],
-                           'E': ['A', 'D']}, start_node='A', expected=res_1),
-    Case(name='№2', input={'A': ['C', 'D'],
-                           'B': ['A', 'S'],
-                           'C': ['B', 'D'],
-                           'D': ['A', 'C', 'E'],
-                           'E': ['A', 'D']}, start_node='A', expected=res_2),
-    Case(name='№3', input={'A': ['C', 'D'],
-                           'B': ['A', 'S'],
-                           'C': ['B', 'Y'],
-                           'D': ['A', 'C', 'E'],
-                           'E': []}, start_node='A', expected=res_3),
-    Case(name='№4', input={'A': ['D'],
-                           'B': ['A', 'S'],
-                           'C': ['B', 'D'],
-                           'D': ['A', 'C', 'E'],
-                           'E': ['A', 'D']}, start_node='A', expected=res_4),
-    Case(name='№5', input={'A': ['C', 'D'],
-                           'B': ['A', 'S'],
-                           'C': ['B', 'D'],
-                           'D': ['B', 'B', 'E'],
-                           'E': ['A', 'D']}, start_node='A', expected=res_5),
-    Case(name='№6', input={'A': ['C', 'D'],
-                           'B': ['A', 'S'],
-                           'C': ['B', 'D'],
-                           'D': ['A', 'C', 'E'],
-                           'E': ['A']}, start_node='A', expected=res_6)
+TEST_CASES_FOR_DFS = [
+    Case(name='№1', input=dict_1, start_node='A', expected=res_1),
+    Case(name='№2', input=dict_2, start_node='A', expected=res_2),
+    Case(name='№3', input=dict_3, start_node='A', expected=res_3),
+    Case(name='№4', input=dict_4, start_node='A', expected=res_4),
+    Case(name='№5', input=dict_5, start_node='A', expected=res_5),
+    Case(name='№6', input=dict_6, start_node='A', expected=res_6)
 ]
 
 
 res_1 = list(nwx.algorithms.traversal.bfs_tree(G_1, 'A'))
 res_2 = list(nwx.algorithms.traversal.bfs_tree(G_2, 'A'))
-res_3 = list(nwx.algorithms.traversal.bfs_tree(G_3, 'C'))
+res_3 = list(nwx.algorithms.traversal.bfs_tree(G_3, 'A'))
 res_4 = list(nwx.algorithms.traversal.bfs_tree(G_4, 'A'))
 res_5 = list(nwx.algorithms.traversal.bfs_tree(G_5, 'A'))
 res_6 = list(nwx.algorithms.traversal.bfs_tree(G_6, 'A'))
 
 # тесты считаются на том же наборе графов, что и dfs
-TEST_CASES_FOR_DFS = [
-    Case(name='№1', input={'A': ['C', 'D'],
-                           'B': [],
-                           'C': ['B', 'D'],
-                           'D': ['A', 'C', 'E'],
-                           'E': ['A', 'D']}, start_node='A', expected=res_1),
-    Case(name='№2', input={'A': ['C', 'D'],
-                           'B': ['A', 'S'],
-                           'C': ['B', 'D'],
-                           'D': ['A', 'C', 'E'],
-                           'E': ['A', 'D']}, start_node='A', expected=res_2),
-    Case(name='№3', input={'A': ['C', 'D'],
-                           'B': ['A', 'S'],
-                           'C': ['B', 'Y'],
-                           'D': ['A', 'C', 'E'],
-                           'E': []}, start_node='A', expected=res_3),
-    Case(name='№4', input={'A': ['D'],
-                           'B': ['A', 'S'],
-                           'C': ['B', 'D'],
-                           'D': ['A', 'C', 'E'],
-                           'E': ['A', 'D']}, start_node='A', expected=res_4),
-    Case(name='№5', input={'A': ['C', 'D'],
-                           'B': ['A', 'S'],
-                           'C': ['B', 'D'],
-                           'D': ['B', 'B', 'E'],
-                           'E': ['A', 'D']}, start_node='A', expected=res_5),
-    Case(name='№6', input={'A': ['C', 'D'],
-                           'B': ['A', 'S'],
-                           'C': ['B', 'D'],
-                           'D': ['A', 'C', 'E'],
-                           'E': ['A']}, start_node='A', expected=res_6)
+TEST_CASES_FOR_BFS = [
+    Case(name='№1', input=dict_1, start_node='A', expected=res_1),
+    Case(name='№2', input=dict_2, start_node='A', expected=res_2),
+    Case(name='№3', input=dict_3, start_node='A', expected=res_3),
+    Case(name='№4', input=dict_4, start_node='A', expected=res_4),
+    Case(name='№5', input=dict_5, start_node='A', expected=res_5),
+    Case(name='№6', input=dict_6, start_node='A', expected=res_6)
 ]
 
 
 @pytest.mark.parametrize('bfs', TEST_CASES_FOR_BFS, ids=str)
-def test_count_steps_to_exit(bfs: Case) -> None:
-    graph = MyGraph(*bfs.input)
-    answer = graph.DFS(*bfs.start_node)
+def test_breadth_first_search(bfs: Case) -> None:
+    graph = MyGraph(bfs.input)
+    answer = graph.BFS(bfs.start_node)
     assert answer == bfs.expected
 
 
 @pytest.mark.parametrize('dfs', TEST_CASES_FOR_DFS, ids=str)
-def test_on_border(dfs: Case) -> None:
-    graph = MyGraph(*dfs.input)
-    answer = graph.BFS(*dfs.start_node)
+def test_depth_first_search(dfs: Case) -> None:
+    graph = MyGraph(dfs.input)
+    answer = graph.DFS(dfs.start_node)
     assert answer == dfs.expected
