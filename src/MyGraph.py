0b2c08e98f49703316484bcccaf35cfacca0a79a
YaroslavAggressive <55752143+YaroslavAggressive@users.noreply.github.com>
2021-05-19 12:49:31 +0500

    Added changes in methods

--- src/MyGraph.py
+++ src/MyGraph.py
@@ -2,10 +2,8 @@ import networkx as nwx
 from collections import deque
 
 
-NODE_NOT_VISITED = 'white'  # indicator of node, which not yet used in the algorithm
-NODE_IN_PROCESS = 'grey'  # indicator of processing current node with dfs/bfs algorithm
-NODE_WORKED = 'black'  # indicator of finish step of dfs/bfs algorithm with current graph node
-
+NODE_NOT_VISITED = 'NV'  # indicator of visited node in bfs the algorithm
+NODE_VISITED = 'V'  # indicator of non-visited node in bfs algorithm
 
 class Graph(nwx.MultiDiGraph):
 
@@ -19,32 +17,28 @@ class GraphTraversal:
     # auxiliary function for dfs main algo
     @staticmethod
     def dfs_recursive(node, graph, nodes_colors, result):
-        nodes_colors[node] = NODE_IN_PROCESS
+        nodes_colors.pop(node)
         result.append(node)
         for node_tmp in graph.neighbors(node):
-            if nodes_colors[node_tmp] == NODE_NOT_VISITED:
+            if node_tmp in nodes_colors:
                 GraphTraversal.dfs_recursive(node_tmp, graph, nodes_colors, result)
-
-        nodes_colors[node] = NODE_WORKED
 
     @staticmethod
     def DFS(graph: Graph, start_node):  # depth search
-        nodes_colors = {node: NODE_NOT_VISITED for node in graph.nodes()}
+        nodes_colors = {node: None for node in graph.nodes()}
         result = []
         GraphTraversal.dfs_recursive(start_node, graph, nodes_colors, result)
         return result
 
     @staticmethod
     def BFS(graph: Graph, start_node):  # breadth search
-        nodes_visited = set()
+        nodes_visited = dict()
         nodes_stack = deque(start_node)
-        result = []
         while nodes_stack:
             curr_node = nodes_stack.popleft()
             if curr_node not in nodes_visited:
-                result.append(curr_node)
-                nodes_visited.add(curr_node)
+                nodes_visited.update({curr_node: None})
             for node in graph.neighbors(curr_node):
                 if node not in nodes_visited:
                     nodes_stack.append(node)
-        return result
+        return list(nodes_visited.keys())
